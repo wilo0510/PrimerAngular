344a86b17802a3a9cd56526659f8bb9500c5909e
wilo <wrpachonl@gmail.com>
2019-08-15 11:29:05 -0500

    Comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//Este import me trae las funcionalidades y acciones pertenecientes a 
+//forms de html
 import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Esta parte hace los imports
+//de cada componente que se usa en 
+//la aplicación
 import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
 import { UserComponent } from './user/user.component';
 import {DatosService} from './datos.service';
+//Este import me permite hacer peticiones get
+//de un servicio http
 import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
--- src/app/datos.service.ts
+++ src/app/datos.service.ts
@@ -1,5 +1,12 @@
+//Este TS se obtiene con el comando
+//ng generate service nombreServicio
 import { Injectable } from '@angular/core';
+//ESte HttpCliente se usa en cada componente
+//que pueda llegar a necesitar un http
 import {HttpClient} from '@angular/common/http'
+/*Este import trae el objeto tipo
+Post para poderlo manipular y pasarlo 
+a esa clase */
 import {Post} from './Post'
 
 @Injectable({
@@ -8,6 +15,8 @@ import {Post} from './Post'
 export class DatosService {
 
   constructor(private httpClient:HttpClient) { 
+    /*Aqui me sercioro que el servicio http esta 
+    funcionando */
     console.log("El servicio funciona");
   }
   obtenerDatos(){
