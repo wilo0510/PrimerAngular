9f7f8f213af24440ce7cac2604949c4ddb775c48
wilo <wrpachonl@gmail.com>
2019-08-14 21:59:33 -0500

    Pruebas con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{HolaMundo} from './primerComponente/primerComponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundo
   ],
   imports: [
     BrowserModule,
---
+++ src/app/primerComponente/primerComponent.ts
@@ -0,0 +1,9 @@
+import {Component}from '@angular/core';
+
+@Component({
+    selector:'HolaMundo',
+    template: '<h1>Hello world</h1>'
+})
+export class HolaMundo{
+
+}
