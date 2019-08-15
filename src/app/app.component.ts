f5491639b3b82beb02b0f403575ba72860a9d91b
wilo <wrpachonl@gmail.com>
2019-08-15 00:06:10 -0500

    Mini crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  users=['yo','Miguel','Puto el que lo lea','No mentiras']
-  activado=true;
+  
+  users:string[]=["Wilo","Miguel","Camila"];
+
+  decirHola(){
+    alert("Que se dice");
+  }
+  agregarUsuario(nombreUsuario){
+    this.users.push(nombreUsuario.value);
+    nombreUsuario.value='';
+    nombreUsuario.focus();
+    return false;
+  }
+  eliminarUsuario(user){
+    for(let i=0;i<this.users.length;i++)
+    {
+      if(user==this.users[i]){
+        this.users.splice(i,1);
+      }
+    }
+  }
+  
 }
